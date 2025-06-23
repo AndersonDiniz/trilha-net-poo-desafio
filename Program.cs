b055bc178bce3dd5d7dd247b376d883791dfee3c
ANDERSON-Findes <165189305+ANDERSON-Findes@users.noreply.github.com>
2025-06-23 10:35:12 -0300

    finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456789", modelo: "Modelo Nokia", imei: "111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\nSmartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "987654321", modelo: "Modelo Iphone", imei: "222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
